740384d5856672976b733d1b689710d4b90aa497
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2024-02-20 11:05:09 +0700

    dsad

--- src/MongoDBCRUD.js
+++ src/MongoDBCRUD.js
@@ -9,5 +9,82 @@ const bodyParser = require("body-parser");
 
 //Database connection
 mongoose.connect(
-    "mongodb://"
-)
+    "mongodb://admin:LBRald64579@node57392-patiphat-noderest.proen.app.ruk-com.cloud:11788",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const Book = mongoose.model("Book", {
+    id: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    title: String,
+    author: String,
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/books", async (req, res) => {
+    try {
+        const lastBook = await Book.findOne().sort({ id: -1 });
+        const nextId = lastBook ? lastBook.id + 1 : 1;
+
+        const book = new Book({
+            id: nextId,
+            ...req.body,
+        });
+        
+        await book.save();
+        res.send(book);
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/books", async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.send(books);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/books/:id" , async (req, res) => {
+    try {
+        const book = await Book.findOne({id:req.params.id});
+        res.send(book);
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
+app.put("/books/:id", async (req, res) => {
+    try {
+        const book = await Book.findOneAndUpdate({id:req.params.id}, req.body, {
+            new: true,
+        });
+        res.send(book); 
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.delete("/books/:id", async (req, res) => {
+    try {
+        const book = await Book.findOneAndDelete({id:req.params.id});
+        res.send(book);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
